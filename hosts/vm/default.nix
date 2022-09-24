8302877a5aaca3fc01fbb3ce1028acf6e155f185
Obscurely <adrian.obscurely@protonmail.com>
2022-09-24 03:15:08 -0400

    add vm host

---
+++ hosts/vm/default.nix
@@ -0,0 +1,85 @@
+{ pkgs, config, lib, ... }:
+{
+  imports = [
+    ../home.nix
+    ./hardware-configuration.nix
+  ];
+
+  ## Modules
+  modules = {
+    desktop = {
+      bspwm.enable = true;
+      apps = {
+        rofi.enable = true;
+        bitwarden.enable = true;
+        utils.enable = true;
+      };
+      browsers = {
+        default = "firefox";
+        chromium.enable = true;
+        firefox.enable = true;
+        qutebrowser.enable = true;
+      };
+      gaming = {
+        steam.enable = true;
+        legendary.enable = true;
+        lutris.enable = true;
+        wine.enable = true;
+      };
+      media = {
+        daw.enable = true;
+        documents.enable = true;
+        graphics.enable = true;
+        mpv.enable = true;
+        recording.enable = true;
+        audio.enable = true;
+      };
+      term = {
+        default = "xst";
+        st.enable = true;
+      };
+      vm = {
+        virtd.enable = true;
+      };
+    };
+    dev = {
+      cc.enable = true;
+      node.enable = true;
+      rust.enable = true;
+      python.enable = true;
+    };
+    editors = {
+      default = "vim";
+      vim.enable = true;
+    };
+    shell = {
+      direnv.enable = true;
+      git.enable    = true;
+      gnupg.enable  = true;
+      tmux.enable   = true;
+      zsh.enable    = true;
+      utils.enable = true;
+    };
+    hardware = {
+      audio.enable = true;
+      fs = {
+        enable = true;
+        ssd.enable = true;
+      };
+    };
+    services = {
+      ssh.enable = true;
+      # docker.enable = true;
+      # Needed occasionally to help the parental units with PC problems
+      # teamviewer.enable = true;
+    };
+    theme.active = "main";
+  };
+
+
+  ## Local config
+  programs.ssh.startAgent = true;
+  services.openssh.startWhenNeeded = true;
+
+  networking.networkmanager.enable = true;
+}

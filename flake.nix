8673f4efcec07dc9ac6d5b4e2d8a948101affaaa
Obscurely <adrian.obscurely@protonmail.com>
2022-10-02 13:32:17 +0300

    remove agenix

--- flake.nix
+++ flake.nix
@@ -17,8 +17,6 @@
       nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";  # for packages on the edge
       home-manager.url = "github:rycee/home-manager/master";
       home-manager.inputs.nixpkgs.follows = "nixpkgs";
-      agenix.url = "github:ryantm/agenix";
-      agenix.inputs.nixpkgs.follows = "nixpkgs";
 
       # Extras
       emacs-overlay.url  = "github:nix-community/emacs-overlay";

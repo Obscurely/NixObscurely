a279063b8901ac6e800ab7ae238ee52c3851300d
Obscurely <adrian.obscurely@protonmail.com>
2022-10-07 21:21:33 +0300

    bump

---
+++ hosts/main-example/default.nix
@@ -0,0 +1,85 @@
+{ pkgs, config, lib, ... }:
+{
+  imports = [
+    ../home.nix
+    ./hardware-configuration.nix
+  ];
+
+  ## Modules
+  modules = {
+    desktop = {
+      bspwm.enable = true;
+      compression.enable = true;
+      apps = {
+        bitwarden.enable = true;
+        discord.enable = true;
+        rofi.enable = true;
+        utils.enable = true;
+      };
+      browsers = {
+        default = "firefox";
+        chromium.enable = true;
+        firefox.enable = true;
+        qutebrowser.enable = true;
+      };
+      gaming = {
+        legendary.enable = true;
+        lutris.enable = true;
+        steam.enable = true;
+        wine.enable = true;
+      };
+      media = {
+        audio.enable = true;
+        documents.enable = true;
+        graphics.enable = true;
+        mpv.enable = true;
+        recording.enable = true;
+      };
+      term = {
+        default = "xterm-kitty";
+        kitty.enable = true;
+      };
+      vm = {
+        virtd.enable = true;
+      };
+    };
+    dev = {
+      arduino.enable = true;
+      cc.enable = true;
+      dotnet.enable = true;
+      go.enable = true;
+      java.enable = true;
+      lua.enable = true;
+      node.enable = true;
+      python.enable = true;
+      rust.enable = true;
+      shell.enable = true;
+      web.enable = true;
+      yaml.enable = true;
+    };
+    editors = {
+      default = "nvim";
+      nvim.enable = true;
+    };
+    hardware = {
+      audio.enable = true;
+      fs = {
+        enable = true;
+        ssd.enable = true;
+      };
+      nvidia.enable = true;
+      razer.enable = true;
+      sensors.enable = true;
+    };
+    shell = {
+      direnv.enable = true;
+      git.enable    = true;
+      gnupg.enable  = true;
+      utils.enable = true;
+      zsh.enable    = true;
+    };
+    theme.active = "main";
+  };
+
+  networking.networkmanager.enable = true;
+}
--- hosts/main/hardware-configuration.nix
+++ hosts/main/hardware-configuration.nix
@@ -5,26 +5,41 @@
 
 {
   imports =
-    [ (modulesPath + "/profiles/qemu-guest.nix")
+    [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/86b0233e-c9e7-43f8-b4ef-b7ecc3215669";
+    { device = "/dev/disk/by-uuid/9d3d0306-a30b-49ef-a2a5-a1cd57d6a628";
       fsType = "f2fs";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/62CA-BED5";
+    { device = "/dev/disk/by-uuid/7DAA-D9A8";
       fsType = "vfat";
     };
 
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/9e602af1-a48b-4682-9e71-2c7da91f5e86";
+      fsType = "f2fs";
+    };
+
+  fileSystems."/data" =
+    { device = "/dev/disk/by-uuid/8e8088eb-c770-48bd-b593-f57fb0a93e6e";
+      fsType = "f2fs";
+    };
+
+  fileSystems."/extra" =
+    { device = "/dev/disk/by-uuid/e34247f9-df30-47c9-9846-5219b6918f35";
+      fsType = "xfs";
+    };
+
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/8642121d-1414-4425-83ec-12e8620bbc72"; }
+    [ { device = "/dev/disk/by-uuid/c552ad1f-9b63-4853-ac4c-77e1f8a13e05"; }
     ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
@@ -32,7 +47,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp8s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;

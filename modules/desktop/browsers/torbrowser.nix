cef240a544b5b3fbf514f43111d2a0f35ce6ab45
Obscurely <adrian.obscurely@protonmail.com>
2022-10-21 21:03:19 +0300

    add tor browser

---
+++ modules/desktop/browsers/torbrowser.nix
@@ -0,0 +1,18 @@
+# modules/browser/torbrowser.nix
+
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop.browsers.torbrowser;
+in {
+  options.modules.desktop.browsers.torbrowser= {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [
+      tor-browser-bundle-bin
+    ];
+  };
+}

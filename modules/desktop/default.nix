8637fbe916a853a0936bb49b0d09af72bb822bab
Obscurely <adrian.obscurely@protonmail.com>
2022-11-07 20:42:22 +0200

    try fix activation script again

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -136,7 +136,60 @@ in {
 
     # Run activation scripts
     system.userActivationScripts.setupUser = ''
-      /etc/dotfiles/scripts/setup_user.sh &
+      # cd into home dir just to make sure
+      cd ~
+
+      # Create xdg-user-dirs
+
+      if ! [ -d "Desktop" ]; then
+        mkdir ~/Desktop
+      fi
+
+      if ! [ -d "Documents" ]; then
+        mkdir ~/Documents
+      fi
+      if ! [ -d "Downloads" ]; then
+        mkdir ~/Downloads
+      fi
+
+      if ! [ -d "Music" ]; then
+        mkdir ~/Music
+      fi
+
+      if ! [ -d "Pictures" ]; then
+        mkdir ~/Pictures
+      fi
+
+      if ! [ -d "Videos" ]; then
+        mkdir ~/Videos
+      fi
+
+      # Create screenshots dir for flameshot
+
+      if ! [ -d "Pictures/screenshots" ]; then
+        mkdir -p ~/Pictures/screenshots
+      fi
+
+      # Create Code dir for programming
+
+      if ! [ -d "Code" ]; then
+        mkdir ~/Code
+      fi
+
+      # Install neovim configs
+
+      if ! [ -d "Code" ]; then
+        mkdir ~/Code
+      fi
+
+      if ! [ -d ".config/nvim/.git" ]; then
+        mkdir ~/.config/nvim # create nvim dir .config in case the installer didn't
+        git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+        git clone https://github.com/Obscurely/neovim.git ~/.config/nvim/lua/custom
+        arduino-cli config init
+      fi
+
+      touch ~/scriprun
     '';
 
     # Clean up leftovers, as much as we can

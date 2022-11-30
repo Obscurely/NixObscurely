a800817c1a7d46773e7e27a3e50d8dee5a4ad4c6
Obscurely <adrian.obscurely@protonmail.com>
2022-11-30 16:25:20 +0200

    add bc pkg

--- modules/shell/utils.nix
+++ modules/shell/utils.nix
@@ -27,6 +27,7 @@ in {
       pngquant # optimize png
       jpegoptim # optimize jpeg
       ghostscript # optimize pdf
+      bc # for optimizing photos
       libheif # to view .heic files
       openssl # cryptographic library
       protonup # for managing proton ge

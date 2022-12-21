f29568bc1c06c7bcf5960d2d4f630abe2a64dfca
Obscurely <adrian.obscurely@protonmail.com>
2022-12-21 10:40:37 +0200

    remove pipewire config, breaks audio

--- modules/hardware/audio.nix
+++ modules/hardware/audio.nix
@@ -15,18 +15,6 @@ in {
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-      config.pipewire = {
-        "context.properties" = [
-          {
-            "link.max-buffers" = 64;
-            "log.level" = 2;
-            "default.clock.rate" = 48000;
-            "default.clock.quantum" = 1024;
-            "default.clock.min-quantum" = 32;
-            "default.clock.max-quantum" = 8192;
-          }
-        ];
-      };
     };
 
     security.rtkit.enable = true;

cbae6daf795ee269d9cad4e3fbe4dfddda1e4321
Obscurely <adrian.obscurely@protonmail.com>
2022-12-21 10:32:06 +0200

    change pipewire audio rate to 192khz

--- modules/hardware/audio.nix
+++ modules/hardware/audio.nix
@@ -15,6 +15,18 @@ in {
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
+      config.pipewire = {
+        "context.properties" = [
+          {
+            "link.max-buffers" = 64;
+            "log.level" = 2;
+            "default.clock.rate" = 192000;
+            "default.clock.quantum" = 1024;
+            "default.clock.min-quantum" = 32;
+            "default.clock.max-quantum" = 8192;
+          }
+        ];
+      };
     };
 
     security.rtkit.enable = true;

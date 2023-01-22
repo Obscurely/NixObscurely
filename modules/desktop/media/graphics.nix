06da98acc3ab9a64292c924258c23205c7d8eebd
Obscurely <adrian.obscurely@protonmail.com>
2023-01-22 11:53:07 +0200

    bump + temp remove broken pkgs

--- modules/desktop/media/graphics.nix
+++ modules/desktop/media/graphics.nix
@@ -30,7 +30,7 @@ in {
       # Replaces photoshop
       (if cfg.raster.enable then [
         krita
-        gimp-with-plugins
+        gimp # TODO: once fixed add back gimp-with-plugins
         gimpPlugins.resynthesizer  # content-aware scaling in gimp
       ] else []) ++
 

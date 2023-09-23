5011050fdc8bf1dd9aba1b759d8f59e94dbc1c7b
Obscurely <adrian.obscurely@protonmail.com>
2023-09-23 07:12:29 +0300

    fix flakes error

--- templates/default.nix
+++ templates/default.nix
@@ -17,18 +17,4 @@
   #   path = ./hosts/homeserver;
   #   description = "A starter hosts/* config for a home server config";
   # };
-
-  # Projects
-  project-discourse-theme = {
-    path = ./project/discourse-theme;
-    description = "A Discourse component or theme";
-  };
-  project-discourse-plugin = {
-    path = ./project/discourse-plugin;
-    description = "A Discourse component or theme";
-  };
-  project-nodejs = {
-    path = ./project/nodejs;
-    description = "NodeJS dev environment";
-  };
 }

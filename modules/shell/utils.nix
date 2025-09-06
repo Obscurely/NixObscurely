398ac4e8f25ff4ccc321fd42c71628596acef9d1
Obscurely <adrian.obscurely@protonmail.com>
2025-09-06 13:47:13 +0300

    remove leftover merge comments

--- modules/shell/utils.nix
+++ modules/shell/utils.nix
@@ -47,11 +47,8 @@ in {
 	  svgcleaner # svg cleaning
 	  lsof # view ports usage
 	  ipcalc # calculate ip ranges
-<<<<<<< HEAD
       claude-code # claude agent
-=======
 	  protonmail-bridge # for using protonmail with thunderbird
->>>>>>> c5a2fde9e73d86cecc8549e672cde6269bd89122
     ];
 
     programs.adb.enable = true;
